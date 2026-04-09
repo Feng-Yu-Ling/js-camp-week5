9b7bb20e9fb3da1f338d501156ccc26311bf9cb5
Feng-Yu-Ling <hisquall0901@yahoo.com.tw>
2026-04-09 22:56:47 +0800

    feat: 完成第五週作業

--- homework.js
+++ homework.js
@@ -56,7 +56,12 @@ const orders = [
  * @returns {Object|null} - 回傳產品物件，找不到回傳 null
  */
 function getProductById(products, productId) {
-  // 請實作此函式
+  /*
+  a || b: 當 a 為真值，回傳 a，否則回傳 b
+  a && b: 當 a 為假值，回傳 a，否則回傳 b
+  a ?? b: 當 a 為 undefined 或 null，回傳 b，否則回傳 a
+  */
+  return products.find((item) => item.id === productId) || null;
 }
 
 /**
@@ -66,7 +71,7 @@ function getProductById(products, productId) {
  * @returns {Array} - 回傳符合分類的產品陣列，若 category 為 '全部' 則回傳全部產品
  */
 function getProductsByCategory(products, category) {
-  // 請實作此函式
+  return (category === "全部") ? (products) : (products.filter((item) => item.category === category));
 }
 
 /**
@@ -76,7 +81,7 @@ function getProductsByCategory(products, category) {
  * 計算方式：Math.round((price / origin_price) * 100) / 10
  */
 function getDiscountRate(product) {
-  // 請實作此函式
+  return `${Math.round((product.price / product.origin_price) * 100) / 10}折`
 }
 
 /**
@@ -85,7 +90,12 @@ function getDiscountRate(product) {
  * @returns {Array} - 回傳分類陣列，例如 ['衣服', '褲子', '鞋子', '配件']
  */
 function getAllCategories(products) {
-  // 請實作此函式
+  // 透過 new Set() 將重複的值移除但保留順序
+  /*
+  Array.from(可迭代物件) 跟 [...可迭代物件] 都能將可迭代物件轉為陣列，
+  但 new Array() 不行，因為這只能建立一個新陣列，無法將可迭代物件一個一個取出並放到陣列中
+  */
+  return Array.from(new Set(products.map((item) => item.category)));
 }
 
 // ========================================
@@ -98,7 +108,7 @@ function getAllCategories(products) {
  * @returns {number} - 回傳數字（原價 × 數量 的總和）
  */
 function calculateCartOriginalTotal(carts) {
-  // 請實作此函式
+  return carts.reduce((sum, item) => sum + (item.product.origin_price * item.quantity), 0);
 }
 
 /**
@@ -107,7 +117,7 @@ function calculateCartOriginalTotal(carts) {
  * @returns {number} - 回傳數字（售價 × 數量 的總和）
  */
 function calculateCartTotal(carts) {
-  // 請實作此函式
+  return carts.reduce((sum, item) => sum + (item.product.price * item.quantity), 0);
 }
 
 /**
@@ -116,7 +126,7 @@ function calculateCartTotal(carts) {
  * @returns {number} - 回傳原價總金額 - 售價總金額
  */
 function calculateSavings(carts) {
-  // 請實作此函式
+  return carts.reduce((sum, item) => sum + ((item.product.origin_price - item.product.price) * item.quantity), 0);
 }
 
 /**
@@ -125,7 +135,7 @@ function calculateSavings(carts) {
  * @returns {number} - 回傳所有商品的 quantity 總和
  */
 function calculateCartItemCount(carts) {
-  // 請實作此函式
+  return carts.reduce((sum, item) => sum + item.quantity, 0)
 }
 
 /**
@@ -135,7 +145,7 @@ function calculateCartItemCount(carts) {
  * @returns {boolean} - 回傳 true 或 false
  */
 function isProductInCart(carts, productId) {
-  // 請實作此函式
+  return carts.some((item) => item.product.id === productId)
 }
 
 // ========================================
@@ -151,7 +161,29 @@ function isProductInCart(carts, productId) {
  * 如果產品已存在，合併數量；如果不存在，新增一筆
  */
 function addToCart(carts, product, quantity) {
-  // 請實作此函式
+  // 因為題目提到不要修改原陣列，所以先宣告一個 newCarts
+  let newCarts;
+  // 透過展開運算子將購物車陣列作淺拷貝
+  /*
+  淺拷貝是指只會複製第一層資料，若裡面還有物件或陣列，拷貝後仍會共用相同資料
+  */
+  newCarts = [...carts];
+  // 如果購物車裡面已經有相同產品（product id 相同）就將該產品數量加上 quantity
+  if (carts.some((item) => item.product.id === product.id)) {
+    // 在新的購物車陣列尋找該產品所在的位置並添加對應數量
+    newCarts.find((item) => item.product.id === product.id).quantity += quantity;
+  }
+  // 如果購物車裡面沒有該產品存在，就將該產品及數量組成物件添加到購物車
+  else {
+    let newCartId = `cart-${carts.length + 1}`;
+    let newCart = {
+      id: newCartId,
+      product: product,
+      quantity: quantity
+    };
+    newCarts.push(newCart);
+  }
+  return newCarts
 }
 
 /**
@@ -162,7 +194,30 @@ function addToCart(carts, product, quantity) {
  * @returns {Array} - 回傳新的購物車陣列，如果 newQuantity <= 0，移除該商品
  */
 function updateCartItemQuantity(carts, cartId, newQuantity) {
-  // 請實作此函式
+  /*
+  雖然題目沒有要求，但通常建議保留原陣列，回傳新陣列
+  */
+  const target = carts.find((item) => item.id === cartId);
+  // 如果沒有找到對應的購物車項目 ID，就回傳原陣列，也就是不管有沒有找到都回傳陣列
+  if (!target) {
+    return carts
+  }
+  else if (newQuantity <= 0) {
+    // 從陣列刪除特定元素可透過 filter
+    return carts.filter((item) => item.id !== cartId)
+  }
+  // 從陣列更新特定元素可用 map
+  return carts.map(function (item) {
+    if (item.id === cartId) {
+      return {
+        ...item,
+        quantity: newQuantity
+      }
+    }
+    else {
+      return item
+    }
+  })
 }
 
 /**
@@ -172,7 +227,20 @@ function updateCartItemQuantity(carts, cartId, newQuantity) {
  * @returns {Array} - 回傳移除後的新購物車陣列
  */
 function removeFromCart(carts, cartId) {
-  // 請實作此函式
+  // 為了不修改原陣列，所以宣告一個 newCarts
+  let newCarts;
+  if (carts.find((item) => item.id === cartId)) {
+    // 從陣列刪除特定元素可透過 filter
+    newCarts = carts.filter((item) => item.id !== cartId);
+  }
+  else {
+    // 透過展開運算子將購物車陣列作淺拷貝
+    /*
+    淺拷貝是指只會複製第一層資料，若裡面還有物件或陣列，拷貝後仍會共用相同資料
+    */
+    newCarts = [...carts]; 
+  }
+  return newCarts
 }
 
 /**
@@ -180,7 +248,15 @@ function removeFromCart(carts, cartId) {
  * @returns {Array} - 回傳空陣列
  */
 function clearCart() {
-  // 請實作此函式
+  // 建議採用 array.length = 0 而不是 array = []
+  /*
+  雖然 array.length = 0 跟 array = [] 都能將回傳空陣列，
+  但 array = [] 的作法是將變數指向一個新的空陣列，原陣列不變
+  如果有其他變數引用原陣列，不會一起清空
+  最佳實踐是透過 array.length = 0，除了清空原陣列，也會一併清除所有引用
+  */
+  carts.length = 0;
+  return carts
 }
 
 // ========================================
@@ -193,7 +269,23 @@ function clearCart() {
  * @returns {number} - 只計算已付款 (paid: true) 的訂單
  */
 function calculateTotalRevenue(orders) {
-  // 請實作此函式
+  // 將已付款的訂單篩選出來
+  const paidOrders = orders.filter((item) => item.paid === true);
+  // 宣告一個總營收變數並指定初始值為零
+  let totalRevenue = 0;
+  /*
+  透過雙層迴圈將已付款的訂單累加總營收
+  內層：將已付款的訂單中的每一筆產品，其 price 乘以 quantity 的結果都進行累加，添加到總營收
+  外層：將每筆已付款的訂單都進行相同過程
+  */
+  // 迴圈的變數以語意化命名而不是簡單的 x 或 i
+  for (const order of paidOrders) {
+    // 迴圈的變數以語意化命名而不是簡單的 x 或 i
+    for (const product of order.products) {
+      totalRevenue += product.price * product.quantity;
+    }
+  }
+  return totalRevenue
 }
 
 /**
@@ -203,7 +295,7 @@ function calculateTotalRevenue(orders) {
  * @returns {Array} - 回傳篩選後的訂單陣列
  */
 function filterOrdersByStatus(orders, isPaid) {
-  // 請實作此函式
+  return orders.filter((item) => item.paid === isPaid)
 }
 
 /**
@@ -219,7 +311,52 @@ function filterOrdersByStatus(orders, isPaid) {
  * }
  */
 function generateOrderReport(orders) {
-  // 請實作此函式
+  // 宣告一個總訂單數量變數
+  const totalOrders = orders.length;
+  // 宣告一個已付款訂單數量變數並指定初始值為零
+  let paidOrders = 0;
+  // 宣告一個未付款訂單數量變數並指定初始值為零
+  let unpaidOrders = 0;
+  // 宣告一個總營收變數並指定初始值為零
+  let totalRevenue = 0;
+  // 宣告一個所有訂單平均金額變數並指定初始值為零
+  let averageOrderValue = 0;
+  // 宣告一個所有訂單金額變數並指定初始值為零
+  let totalOrderValue = 0;
+
+  // 迴圈的變數以語意化命名而不是簡單的 x 或 i
+  for (const order of orders) {
+    // 宣告一個訂單金額變數並指定初始值為零
+    let orderValue = 0;
+    // 迴圈的變數以語意化命名而不是簡單的 x 或 i
+    for (const product of order.products) {
+      // 訂單金額等於產品價格乘以產品數量
+      orderValue += product.price * product.quantity;
+    }
+    // 將該筆訂單金額添加到所有訂單金額
+    totalOrderValue += orderValue;
+
+    // 如果該筆訂單已付款，則已付款訂單數量加一，同時將該筆訂單金額添加到總營收
+    if (order.paid) {
+      paidOrders ++;
+      totalRevenue  += orderValue;
+    }
+    // 如果該筆訂單未付款，則未付款訂單數量加一
+    else {
+      unpaidOrders ++;
+    }
+  }
+
+  // 所有訂單平均金額等於所有訂單金額除以總訂單數量，但如果總訂單數量為零則回傳零
+  averageOrderValue =  (totalOrders) ? (totalOrderValue / totalOrders) : 0
+
+  return {
+    totalOrders,
+    paidOrders,
+    unpaidOrders,
+    totalRevenue,
+    averageOrderValue
+  }
 }
 
 /**
@@ -232,7 +369,19 @@ function generateOrderReport(orders) {
  * }
  */
 function groupOrdersByPayment(orders) {
-  // 請實作此函式
+  // 宣告一個結果變數並指定初始值為空物件
+  const result = {};
+  for (const order of orders) {
+    // 宣告一個付款方式變數並指定為該訂單的 user 的 payment
+    const payment = order.user.payment;
+    // 如果結果變數中尚未有該付款方式的屬性，就先指定為空陣列
+    if (!result[payment]) {
+      result[payment] = [];
+    }
+    // 將該訂單資料添加到結果變數中該付款方式的屬性陣列
+    result[payment].push(order);
+  }
+  return result
 }
 
 // ========================================
